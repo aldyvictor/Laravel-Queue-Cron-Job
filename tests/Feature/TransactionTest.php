1d8ea4eabec25c473fdad957b0ba00e8a08189bf
Aldy Victor Rianes <aldyvictor1046@gmail.com>
2022-01-07 09:56:07 +0700

    feat ( Testing get user history )

--- tests/Feature/TransactionTest.php
+++ tests/Feature/TransactionTest.php
@@ -3,12 +3,16 @@
 namespace Tests\Feature;
 
 use App\Models\Transaction;
+use App\Models\User;
+use App\Models\Product;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
 class TransactionTest extends TestCase
 {
+    use DatabaseMigrations;
     /**
      * A basic feature test example.
      *
@@ -24,7 +28,36 @@ class TransactionTest extends TestCase
             'product_image_url' => 'null',
             'expired_at' => '2022-01-23 08:23:23',
             'amount' => 200000
-        ]))->assertCreated();
+        ]))->assertOk();
+    }
 
+    public function test_get_user_history()
+    {
+        $user = User::factory()->create();
+        $product = Product::create([
+            'name' => 'tomat',
+            'price' => 10000,
+            'product_image_original_name' => 'null',
+            'product_image_original_url' => 'null',
+            'product_image_large_name' => 'null',
+            'product_image_large_url' => 'null',
+            'product_image_medium_name' => 'null',
+            'product_image_medium_url' => 'null',
+            'product_image_small_name' => 'null',
+            'product_image_small_url' => 'null',
+        ]);
+        $transaction = Transaction::create([
+            'user_id' => $user->id,
+            'product_id' => $product->id,
+            'product_qty' => 20,
+            'product_image_name' => 'null',
+            'product_image_url' => 'null',
+            'expired_at' => '2022-01-23 08:23:23',
+            'amount' => 200000
+        ]);
+        $response  = $this->actingAs($user)->get('api/transaction-history/'. $user->id)
+                        ->assertOk();
+
+        $this->assertEquals($transaction->user_id, $user->id);
     }
 }
